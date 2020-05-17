934433f67a4d892d8c7aa371d05934e8632d6567
PahomovSasha <caxap13@mail.ru>
2020-05-17 18:33:05 +0500

    Крестики-нолики на JavaFX [#258820]

--- tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
@@ -1,5 +1,6 @@
 package job4j.tictactoe;
 
+import java.util.Arrays;
 import java.util.function.Predicate;
 
 public class Logic3T {
@@ -9,6 +10,16 @@ public class Logic3T {
         this.table = table;
     }
 
+    /**
+     * Метод проверяет выигрышные комбинации
+     *
+     * @param predicate проверка Крестика или Нолика
+     * @param startX начальная точка по горизонтали
+     * @param startY начальная точка по вертикали
+     * @param deltaX движение по горизонтали
+     * @param deltaY движение по вертикали
+     * @return если выигрышные комбинации есть - true, иначе false;
+     */
     public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
         boolean result = true;
         for (int index = 0; index != this.table.length; index++) {
@@ -23,38 +34,75 @@ public class Logic3T {
         return result;
     }
 
+    /**
+     * Метод вызывает проверку выигрышных комбинации для Крестика
+     *
+     * @return если выигрышные комбинации есть - true, иначе false;
+     */
     public boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1);
+        return this.isWin(Figure3T::hasMarkX);
     }
 
+    /**
+     * Метод вызывает проверку выигрышных комбинации для Нолика
+     *
+     * @return если выигрышные комбинации есть - true, иначе false;
+     */
     public boolean isWinnerO() {
-        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 2, 1, 0) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 2, 0, 0, 1) ||
-                this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1) ||
-                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1);
+        return this.isWin(Figure3T::hasMarkO);
     }
 
-    public boolean hasGap() {
-        boolean result = false;
-        for (Figure3T[] figure3TS : table) {
-            for (Figure3T figure3T : figure3TS) {
-                if (!figure3T.hasMarkO() && !figure3T.hasMarkX()) {
-                    result = true;
-                    break;
-                }
+    /**
+     * Метод вызывает проверки на выигрыш по диагонали и вертикали/горизонтали
+     *
+     * @param predicate проверка Крестика или Нолика
+     * @return Если имеется выигрыш по диагонали или вертикали/горизонтали - true, иначе false
+     */
+    public boolean isWin(Predicate<Figure3T> predicate) {
+        return this.isWinDiagonal(predicate) ||
+                this.isWinVerticalAndHorizontal(predicate);
+    }
+
+    /**
+     * Метод вызывает проверки выигрыша по диагонали
+     *
+     * @param predicate проверка Крестика или Нолика
+     * @return Если имеется выигрыш по диагонали - true, иначе false
+     */
+    public boolean isWinDiagonal(Predicate<Figure3T> predicate) {
+        return this.fillBy(predicate, 0, 0, 1, 1) ||
+                this.fillBy(predicate, this.table.length - 1, 0, -1, 1);
+    }
+
+    /**
+     * Метод вызывает проверки выигрыша по горизонтали и вертикали
+     *
+     * @param predicate проверка Крестика или Нолика
+     * @return Если имеется выигрыш по горизонтали или вертикали - true, иначе false
+     */
+    public boolean isWinVerticalAndHorizontal(Predicate<Figure3T> predicate) {
+        boolean rst = false;
+        for (int i = 0; i < this.table.length; i++) {
+            if (this.fillBy(predicate, i, 0, 0, 1)) {
+                rst = true;
+                break;
+            }
+            if (this.fillBy(predicate, 0, i, 1, 0)) {
+                rst = true;
+                break;
             }
         }
-        return result;
+        return rst;
+    }
+
+    /**
+     * Метод проверяет, если ли пустые клетки для новых ходов
+     *
+     * @return если пустые клетки есть - true, иначе false
+     */
+    public boolean hasGap() {
+        return Arrays.stream(this.table)
+                .flatMap(Arrays::stream)
+                .anyMatch(figure3T -> !figure3T.hasMarkO() && !figure3T.hasMarkX());
     }
 }
